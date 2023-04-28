93187c95cda73ec031cc24dae00412aaa372c8c3
Seraphin Berger <82591988+MCmoderSD@users.noreply.github.com>
2023-04-28 10:16:19 +0200

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,1 +1,11 @@
-sudo git pull https://github.com/MCmoderSD/Cola-Webpage.git
+#!/bin/bash
+
+if [ "$1" = "-auto" ]; then
+  while true; do
+    sudo git -C /var/www/mcmodersd.live/Cola-Webpage pull
+    sleep 30
+  done
+else
+  sudo git -C /var/www/mcmodersd.live/Cola-Webpage pull
+  sudo systemctl reload apache2
+fi
